2d572c91ba350afcac59e4549fe2019d1a135bc8
YashPatankar483 <yashpatankar483@gmail.com>
2024-12-28 11:57:48 +0530

    hello.py mod3

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return "Hello, We are Tom Riddler."
+  return "The jenkins CICD pipeline is working"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
